cf7295048a808638b8f40436758cc2a91185b059
Divyaraj-Mandale <111412336+Divyaraj-Mandale@users.noreply.github.com>
2022-08-29 09:06:28 +0530

    Update Locationweb8Application.java

--- src/main/java/com/locationweb/Locationweb8Application.java
+++ src/main/java/com/locationweb/Locationweb8Application.java
@@ -6,13 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
- @Controller
 public class Locationweb8Application {
-
-	@GetMapping("/show3")
-	public String showLocationPage() {
-		return "create_Location";
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Locationweb8Application.class, args);
